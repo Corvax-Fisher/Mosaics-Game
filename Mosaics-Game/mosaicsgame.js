e3329a4d5ffbaa4a2e841a1c5a69da4386c27242
Corvax-Fisher <martin.heller.mail@gmail.com>
2015-01-18 22:07:43 +0100

    index.html:  - addition: inserted a drop down list that can optionally be replaced with the radio buttons (commented <select> element)  - fix: put <td> tags into <table> and <tr> element where it belongs to  - style: corrected tag indentations  - fix: browser will now be forced not to reload the page from cache  - change: localization mosaicsgame.js:  - new functions: validatePoint, pointOffset, and line  - change: radio buttons will be disabled instead of set invisible  - fixed: width and height of svg will now be calculated correctly  - fixed: pattern will now be generated correctly  - change: slightly adjusted height of the grid numbers  - change: commented grid size (didn't fit with cell size of 20 px)  - fix: "fill" attribute merged into "style" attribute (css)  - TODO: implement validateLine, triangle and validateTriangle functions

--- Mosaics-Game/mosaicsgame.js
+++ Mosaics-Game/mosaicsgame.js
@@ -16,16 +16,48 @@ function validateColor(color)
 {
 	return validColors.indexOf(color) != -1;
 }
+
+function validatePoint(p) {
+	var validYParts = 	["t","c","b"];
+	var validXParts = 	["l","c","r"];
 	
-function setGridSize(col,row)
-{	//Hide Radio Button
-	document.getElementById("GridSelect").style.visibility="hidden";
+	if(	(validYParts.indexOf(p[0]) != -1 && validXParts.indexOf(p[1]) != -1) ||
+		(validYParts.indexOf(p[1]) != -1 && validXParts.indexOf(p[0]) != -1 ) )
+		return true;
+	else return false;
+}
+	
+function setGridSize()
+{	
+	//Hide Radio Button
+	//document.getElementById("GridSelect").style.visibility="hidden";
+	
+	
+	//If drop down list is used
+//	var selectedValue = document.getElementById("gridSelect").value;
+//	document.getElementById("gridSelect").disabled = true;
+	//End If
+	
+	//If radio buttons are used
+	var gridSelect = document.getElementById("gridSelect");
+	var selectedValue;
+	for(var i=0 ; i < gridSelect.elements.length;i++) {
+		gridSelect.elements.item(i).disabled = true;
+		if(gridSelect.elements.item(i).checked) 
+			selectedValue = gridSelect.elements.item(i).value;
+	}
+	//End If
+		
+	bounds = selectedValue.split("x");
+	bounds[0] = Number(bounds[0]);
+	bounds[1] = Number(bounds[1]);
+	
 	//Check count of Rows for cellsize
-	if (col > 12 || row > 12) cellsize=20
+	if (bounds[0] > 12 || bounds[1] > 12) cellsize=20
 	else cellsize=40;
 		
-	bounds[0] = col;
-	bounds[1] = row;
+//	bounds[0] = col;
+//	bounds[1] = row;
 	
 	var strValidColors = "";
 	for(var i = 0; i < validColors.length; i++)
@@ -33,11 +65,12 @@ function setGridSize(col,row)
 	document.getElementById("clrs").innerHTML = 
 		"Verf&uumlgbare Farben: <br>" + strValidColors;
 	
-	//set SVG-Canvas and pattern
+	//Set SVG-Canvas attributes
 	var svg = document.getElementsByTagName("svg")[0];
-	svg.setAttribute("width",cellsize*(bounds[0]+1)+2);
-	svg.setAttribute("height",cellsize*(bounds[0]+1)+2);
-//	var defs = svg.getElementsByTagName("defs")[0];
+	svg.setAttribute("width",cellsize*(Number(bounds[0])+1)+2);
+	svg.setAttribute("height",cellsize*(Number(bounds[1])+1)+2);
+
+	//Set pattern attributes
 	var pattern = svg.getElementById("pattern1");
 	pattern.setAttribute("width",cellsize);
 	pattern.setAttribute("height",cellsize);
@@ -46,12 +79,12 @@ function setGridSize(col,row)
 	var hline = svg.getElementById("hline");
 	hline.setAttribute("x2",cellsize);
 		
-	//Rect around pattern
+	//Set bounding rect attributes
 	var rect = svg.getElementsByTagName("rect")[0];
 	rect.setAttribute("x",cellsize);
 	rect.setAttribute("y",cellsize);
 	rect.setAttribute("width",cellsize*bounds[0]+2);
-	rect.setAttribute("height",cellsize*bounds[0]+2);
+	rect.setAttribute("height",cellsize*bounds[1]+2);
 	
 	//Iterate Grid Numbers
 	var number;
@@ -62,7 +95,7 @@ function setGridSize(col,row)
 		pos = cellToPos(i+1, 0);
 		number.setAttribute("id", "n"+(i+1)+0);
 	    number.setAttribute("x",pos[0]+cellsize/2);
-	    number.setAttribute("y",pos[1]+cellsize*0.6);
+	    number.setAttribute("y",pos[1]+cellsize*0.7);
 	    number.setAttribute("text-anchor","middle");
 	    number.appendChild(document.createTextNode(i+1));
 	    svg.appendChild(number);
@@ -73,24 +106,24 @@ function setGridSize(col,row)
 		pos = cellToPos(0,i+1);
 		number.setAttribute("id", "n"+0+(i+1));
 	    number.setAttribute("x",pos[0]+cellsize/2);
-	    number.setAttribute("y",pos[1]+cellsize*0.6);
+	    number.setAttribute("y",pos[1]+cellsize*0.7);
 	    number.setAttribute("text-anchor","middle");
 	    number.appendChild(document.createTextNode(i+1));
 	    svg.appendChild(number);
 	}
 	
 	//GridSizeNumber
-	index = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	index.setAttribute("id", "index");
-	pos = cellToPos(bounds[0],bounds[1]);
-	index.setAttribute("x", 10);
-	index.setAttribute("y", 15);
-	index.setAttribute("style","font-size:10px");
-	index.setAttribute("style","fill:red");
-	index.setAttribute("text-anchor","right");
-	index.appendChild(document.createTextNode(bounds[0] +" x "  + bounds[1]));
-	svg.appendChild(index);
-	
+//	index = document.createElementNS("http://www.w3.org/2000/svg", "text");
+//	index.setAttribute("id", "index");
+//	pos = cellToPos(bounds[0],bounds[1]);
+//	index.setAttribute("x", 10);
+//	index.setAttribute("y", 15);
+//	index.setAttribute("style","font-size:10px");
+//	index.setAttribute("style","fill:red");
+//	index.setAttribute("text-anchor","right");
+//	index.appendChild(document.createTextNode(bounds[0] +" x "  + bounds[1]));
+//	svg.appendChild(index);
+//	
 }
 	
 function test() {
@@ -103,18 +136,57 @@ function cellToPos(col, row) {
 	return [col*cellsize,row*cellsize];
 }
 
+function pointOffset(p) {
+	var validYParts = 	["t","c","b"];
+	var validXParts = 	["l","c","r"];
+	var offsets = 		[ 2, cellsize/2, cellsize ];
+	var pOffset = 		[0,0];
+	
+	if(p=="cc") pOffset = [offsets[1],offsets[1]];
+	else if(validYParts.indexOf(p[0]) != -1 && validXParts.indexOf(p[1]) != -1 ) {
+		pOffset[0] = offsets[validXParts.indexOf(p[1])];
+		pOffset[1] = offsets[validYParts.indexOf(p[0])];
+	} else if(validYParts.indexOf(p[1]) != -1 && validXParts.indexOf(p[0]) != -1 ) {
+		pOffset[0] = offsets[validXParts.indexOf(p[0])];
+		pOffset[1] = offsets[validYParts.indexOf(p[1])];
+	}
+	return pOffset;
+}
+
+function line(col, row, p1, p2, clr) {
+    var root = document.getElementsByTagName("svg")[0];
+    var child = root.getElementById("e"+col+row);
+    if(child) root.removeChild(child);
+    var element = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    var pos = cellToPos(col, row);
+    
+    p1Offset = pointOffset(p1);
+    p2Offset = pointOffset(p2);
+	
+    element.setAttribute("id", "e"+col+row);
+    element.setAttribute("x1",p1Offset[0] + pos[0]);
+    element.setAttribute("y1",p1Offset[1] + pos[1]);
+    element.setAttribute("x2",p2Offset[0] + pos[0]);
+    element.setAttribute("y2",p2Offset[1] + pos[1]);
+//    element.setAttribute("fill",clr);
+    element.style.stroke = clr;
+    element.style.strokeWidth = 2;
+    root.appendChild(element);
+}
+
 function circle(col,row, clr) {
     var root = document.getElementsByTagName("svg")[0];
     var child = root.getElementById("e"+col+row);
     if(child) root.removeChild(child);
-    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var element = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     var pos = cellToPos(col, row);
-    circle.setAttribute("id", "e"+col+row);
-    circle.setAttribute("r",cellsize/2 - 1);
-    circle.setAttribute("cx",cellsize/2 + 1 + pos[0]);
-    circle.setAttribute("cy",cellsize/2 + 1 + pos[1]);
-    circle.setAttribute("fill",clr);
-    root.appendChild(circle);
+    element.setAttribute("id", "e"+col+row);
+    element.setAttribute("r",cellsize/2 - 1);
+    element.setAttribute("cx",cellsize/2 + 1 + pos[0]);
+    element.setAttribute("cy",cellsize/2 + 1 + pos[1]);
+//    element.setAttribute("fill",clr);
+    element.style.fill = clr;
+    root.appendChild(element);
 }
 
 function square(col,row,clr) {
@@ -128,7 +200,8 @@ function square(col,row,clr) {
     element.setAttribute("y",pos[1]+2);
     element.setAttribute("width",cellsize-2);
     element.setAttribute("height",cellsize-2);
-    element.setAttribute("fill",clr);
+//    element.setAttribute("fill",clr);
+    element.style.fill = clr;
     root.appendChild(element);
 }
 
@@ -154,6 +227,16 @@ function draw(form){
 			document.getElementById("err").innerHTML = "Ung&uumlltige Position.";
 		} else {
 			params[params.length-1] = params[params.length-1].replace(")","");
+			if(cmd[0] == "line") {
+				if(params.length == 5) {
+					if(validatePoint( params[2].toLowerCase() ) && validatePoint( params[3].toLowerCase() ) ) {
+						if(validateColor(params[4]))
+							line(params[0],params[1],params[2].toLowerCase(),params[3].toLowerCase(),params[4]);
+						else document.getElementById("err").innerHTML = "Ung&uumlltige Farbe.";						
+					} else document.getElementById("err").innerHTML = "Ung&uumlltige Punkte.";
+				}
+				else document.getElementById("err").innerHTML = "Parameteranzahl ung&uumlltig (3 erwartet, "+params.length+" bekommen).";
+			}
 			if(cmd[0] == "square" ) {
 				if(params.length == 3) {
 					if(validateColor(params[2]))
